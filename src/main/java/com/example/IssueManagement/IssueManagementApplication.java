a9b8e9ce5f95e3abbe0c6458f95ea89cdfea2678
erdasonur <onur.erdas03@gmail.com>
2021-01-10 15:53:37 +0300

    Mevcut API'lere test datası oluşturuldu. JsonPopulator ile uygulandı.

--- src/main/java/com/example/IssueManagement/IssueManagementApplication.java
+++ src/main/java/com/example/IssueManagement/IssueManagementApplication.java
@@ -6,6 +6,9 @@ import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 public class IssueManagementApplication {
@@ -19,6 +22,13 @@ public class IssueManagementApplication {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		return modelMapper;
+	}
+
+	@Bean
+	public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator(){
+		Jackson2RepositoryPopulatorFactoryBean factoryBean = new Jackson2RepositoryPopulatorFactoryBean();
+		factoryBean.setResources(new Resource[]{new ClassPathResource("project.json"), new ClassPathResource("issue.json")});
+		return factoryBean;
 	}
 
 }
